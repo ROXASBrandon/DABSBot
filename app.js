44d6b8cfc7ccfe5ff4efc7a94334676d0594956a
ROXASBrandon <roxasbrandon@gmail.com>
2021-03-30 14:23:38 -0400

    Added a Minecraft Server Status command

--- app.js
+++ app.js
@@ -31,3 +31,36 @@ client.on("message", (msg) => {
     msg.channel.send(jokes[Math.floor(Math.random() * jokes.length)]);
   }
 });
+
+// Minecraft Server Status
+var request = require('request');
+var mcCommand = '/serverstatus'; // Command for triggering the request on Discord
+var mcIP = '45.43.13.210';
+var mcPort = 25674;
+
+client.on('message', message => {
+    if (message.content === mcCommand) {
+        var url = 'http://api.mcsrvstat.us/2/' + mcIP;
+        request(url, function(err, response, body) {
+            if(err) {
+                console.log(err);
+                return message.reply('Error getting Minecraft server status...');
+            }
+            body = JSON.parse(body);
+            var status = '*Minecraft server is currently offline*';
+            if(body.online) {
+                status = 'Server is Online!\n';
+                status += 'IP: ' + body.ip + "\n";
+                status += 'Port: ' + body.port + "\n";
+                status += 'Connect IP: ' + body.ip + ':' + body.port + "\n"; 
+                status += 'MOTD: ' + body.motd.clean + '\n';
+                if(body.players.online > 0) {
+                    status += body.players.online + '/' + body.players.max + ' people are playing! \nPlayers online: ' + body.players.list;
+                } else {
+                    status += 'Nobody is playing!';
+                }
+            }
+            message.reply(status);
+        });
+    }
+});
